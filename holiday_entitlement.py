c94b5dbf5ae33fac5ea6f8738f2536a9a7e1774b
Lottie Thomas <lottiethomas94@hotmail.co.uk>
2026-01-25 15:22:53 +0000

    Entitlement can be different between a range of dates

--- holiday_entitlement.py
+++ holiday_entitlement.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from enum import Enum
+from typing import Optional
+
 from pydantic import BaseModel
 
 import holidays
@@ -7,15 +9,23 @@ import holidays
 
 class HolidayEntitlement(BaseModel):
     """Class for tracking the holiday entitlement of an individual"""
+    # TODO validation
 
     class CountingMethod(Enum):
         DAYS = 'DAYS'
         HOURS = 'HOURS'
 
+    class AmountException(BaseModel):
+        start_date: datetime
+        end_date: datetime
+        amount: float
+
     counting_method: CountingMethod
     amount: float
     working_pattern: tuple[int, int, int, int, int, int, int]
     bank_holidays_counted: bool
+    renewal_date: datetime
+    amount_exceptions: Optional[list[AmountException]] = None
 
     _bank_holidays = holidays.country_holidays('UK', subdiv='ENG')
 
@@ -24,3 +34,14 @@ class HolidayEntitlement(BaseModel):
             if day in self._bank_holidays:
                 return 0
         return self.working_pattern[day.weekday()]
+
+    def get_entitlement_for_date(self, date: datetime) -> float:
+        if self.amount_exceptions is None:
+            return self.amount
+        else:
+            # If the date is within an exception range, return the exception amount
+            # Otherwise return the entitlement amount
+            for exception in self.amount_exceptions:
+                if exception.start_date <= date <= exception.end_date:
+                    return exception.amount
+            return self.amount
