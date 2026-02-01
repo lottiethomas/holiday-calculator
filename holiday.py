f6390f38cfdcdb0ae9192ab7c008c84e2029b00e
Lottie Thomas <lottiethomas94@hotmail.co.uk>
2026-02-01 11:19:33 +0000

    Calculate the cost of a holiday to a user

---
+++ holiday.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from pydantic import BaseModel
+import pandas as pd
+
+
+class Holiday(BaseModel):
+    start_date: datetime
+    end_date: datetime
+
+    def get_dates_in_holiday(self):
+        return pd.date_range(self.start_date, self.end_date, freq='D')
--- user.py
+++ user.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 
+from holiday import Holiday
 from holiday_entitlement import HolidayEntitlement
 
 
 class User(BaseModel):
     holiday_entitlement: HolidayEntitlement
+    holidays: list[Holiday]
+
+    def get_cost_for_holiday(self, holiday: Holiday) -> float:
+        cost = 0
+        for date in holiday.get_dates_in_holiday():
+            cost += self.holiday_entitlement.get_cost_of_day(date)
+        return cost
