e6f31e91e02ed30b6e5f7b0f12d63aa2edf0431a
Lottie Thomas <lottiethomas94@hotmail.co.uk>
2026-05-03 14:38:47 +0100

    WIP

--- tests/test_user.py
+++ tests/test_user.py
@@ -25,5 +25,39 @@ def user(holiday, holiday_entitlement):
 
 
 def test_get_cost_for_holiday(user, mocker):
+    # Given a user with a holiday that is 14 days long
+    # And that each day costs 1
     mocker.patch('user.HolidayEntitlement.get_cost_of_day', return_value=1)
+    # When the cost of the holiday is retrieved
+    # Then the cost will be 14
     assert user.get_cost_for_holiday(user.holidays[0]) == 14
+
+
+def test_get_cost_of_all_holidays_in_date_range(holiday_entitlement, mocker):
+    # Given a user with two holidays where both fall within the date range
+    user = User(holiday_entitlement=holiday_entitlement,
+                holidays=[
+                    Holiday(start_date=datetime(2026, 4, 1), end_date=datetime(2026, 4, 14)),
+                    Holiday(start_date=datetime(2026, 8, 11), end_date=datetime(2026, 8, 20))
+                ])
+    # And that each day costs 1
+    mocker.patch('user.HolidayEntitlement.get_cost_of_day', return_value=1)
+    # When the cost of holiday between 2026/01/01 and 2026/12/31 is retrieved
+    # Then the cost will be the sum of the two holidays
+    assert user.get_cost_for_holidays_in_date_range(start_date=datetime(2026, 1, 1),
+                                                    end_date=datetime(2026, 12, 31)) == 24
+
+
+def test_get_cost_of_all_holidays_in_range_ignores_out_of_range(holiday_entitlement, mocker):
+    # Given a user with two holidays where one falls outside the date range
+    user = User(holiday_entitlement=holiday_entitlement,
+                holidays=[
+                    Holiday(start_date=datetime(2026, 4, 1), end_date=datetime(2026, 4, 14)),
+                    Holiday(start_date=datetime(2027, 8, 11), end_date=datetime(2027, 8, 20))
+                ])
+    # And that each day costs 1
+    mocker.patch('user.HolidayEntitlement.get_cost_of_day', return_value=1)
+    # When the cost of holiday between 2026/01/01 and 2026/12/31 is retrieved
+    # Then the cost will be only the holiday that falls in the range
+    assert user.get_cost_for_holidays_in_date_range(start_date=datetime(2026, 1, 1),
+                                                    end_date=datetime(2026, 12, 31)) == 14
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 
 from holiday import Holiday
 from holiday_entitlement import HolidayEntitlement
@@ -14,3 +15,19 @@ class User:
         for date in holiday.get_dates_in_holiday():
             cost += self.holiday_entitlement.get_cost_of_day(date)
         return cost
+
+    def get_cost_for_holidays_in_date_range(self, start_date: datetime, end_date: datetime) -> float:
+        cost = 0
+        for holiday in self.holidays:
+            if start_date <= holiday.start_date <= end_date:
+                print(f"Holiday {holiday.description} costs {self.get_cost_for_holiday(holiday)}")
+                cost += self.get_cost_for_holiday(holiday)
+        return cost
+
+    def get_cost_for_holidays_in_holiday_year_starting_in(self, year: int) -> float:
+        start_date = datetime(year, self.holiday_entitlement.renewal_month, 1)
+        end_date = datetime(year + 1, self.holiday_entitlement.renewal_month, 1) - timedelta(days=1)
+        return self.get_cost_for_holidays_in_date_range(start_date, end_date)
+
+    def get_remaining_allowance_for_year_starting_in(self, year: int) -> float:
+        return self.holiday_entitlement.get_entitlement_for_date(datetime(year, self.holiday_entitlement.renewal_month, 1)) - self.get_cost_for_holidays_in_holiday_year_starting_in(year)
