a7a9fa19a35fa1399e63ce03522297f4a3d0b47a
Lottie Thomas <lottiethomas94@hotmail.co.uk>
2026-07-12 11:27:20 +0100

    Use Field to initialize holiday list in UserDto

--- dtos/user_dto.py
+++ dtos/user_dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from dtos.holiday_dto import HolidayDto
 from dtos.holiday_entitlement_dto import HolidayEntitlementDto
@@ -7,9 +7,10 @@ from user import User
 
 class UserDto(BaseModel):
     holiday_entitlement: HolidayEntitlementDto
-    holidays: list[HolidayDto] = []
+    holidays: list[HolidayDto] = Field(default_factory=list)
 
-    def to_user(self) -> 'User':
-        return User(holiday_entitlement=self.holiday_entitlement.to_holiday_entitlement(),
-                    holidays=[holiday_dto.to_holiday() for holiday_dto in self.holidays])
-    
+    def to_user(self) -> "User":
+        return User(
+            holiday_entitlement=self.holiday_entitlement.to_holiday_entitlement(),
+            holidays=[holiday_dto.to_holiday() for holiday_dto in self.holidays],
+        )
